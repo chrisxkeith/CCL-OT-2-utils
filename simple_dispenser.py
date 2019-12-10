7cf4d46cf50a7768ebad944bd8f9a29cae7f979c
CK <chris.keith@gmail.com>
2019-12-10 12:38:24 -0800

    Parameterize more variables. Make code more consistent.

--- simple_dispenser.py
+++ simple_dispenser.py
@@ -7,11 +7,11 @@ metadata = {
     'author': '<chris.keith@gmail.com>'
 }
 
-micro_liters = 1
-
 def run_custom_protocol(pipette_type: 'StringSelection...'='p300-Single',
                         source_labware_type: 'StringSelection...'='trough-12row',
-                        destination_labware_type: 'StringSelection...'='96-flat'):
+                        destination_labware_type: 'StringSelection...'='96-flat',
+                        micro_liters: 'NumberSelection...'=1,
+                        new_tip_type: 'StringSelection...'='once'):
     if pipette_type == 'p300-Single':
         tiprack = labware.load('tiprack-200ul', '1')
         pipette = instruments.P300_Single(
@@ -27,19 +27,24 @@ def run_custom_protocol(pipette_type: 'StringSelection...'='p300-Single',
         pipette = instruments.P10_Single(
             mount='right',
             tip_racks=[tiprack])
+    else:
+        print('Unknown pipette type: ' + pipette_type)
+        return
 
     source_container = labware.load(source_labware_type, '2')
-    output_container = labware.load(destination_labware_type, '3')
+    destination_container = labware.load(destination_labware_type, '3')
 
-    destination_wells = ['A1']
-    source_well = source_container.wells('A1')
+    source_wells = source_container.wells('A1')
+    destination_wells = destination_container.wells('A1')
 
     pipette.distribute(
         micro_liters,
-        source_well,
-        output_container.wells(destination_wells),
-        new_tip='once')
+        source_wells,
+        destination_wells,
+        new_tip=new_tip_type)
 
 run_custom_protocol(**{'pipette_type': 'p300-Single',
                         'source_labware_type': 'trough-12row',
-                        'destination_labware_type': '96-flat'})
+                        'destination_labware_type': '96-flat',
+                        'micro_liters': 1,
+                        'new_tip_type' : 'once'})
